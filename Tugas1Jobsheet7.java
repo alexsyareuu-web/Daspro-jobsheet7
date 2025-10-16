599585580ebc7ec6fa3f78801e086f25b813058c
alexsyareuu-web <alexsyareuu@gmail.com>
2025-10-16 15:50:52 +0700

    new

--- Tugas1Jobsheet7.java
+++ Tugas1Jobsheet7.java
@@ -11,17 +11,20 @@ import java.util.Scanner;
            int jumlahTiket = input.nextInt();
             if (jumlahTiket == 0) {
             break;
-            } else if (jumlahTiket < 0){
-continue;
-} int hargaTiket = 50000;
+            } else if (jumlahTiket < 0) {
+            continue;
+
+            } int hargaTiket = 50000;
             if (jumlahTiket > 10) {
         hargaTiket *= 0.85;
             } else if (jumlahTiket > 4) {
             hargaTiket *= 0.9;
             } totalTiket += jumlahTiket;
+
            totalPendapatan += hargaTiket * jumlahTiket;
            System.out.println("Total tiket terjual: " + totalTiket);
            System.out.println("Total pendapatan: Rp " + totalPendapatan);
+           
         } input.close();
     }
 }
