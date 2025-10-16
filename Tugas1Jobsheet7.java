cf8d9962ee247f40172dc5c79be313c72daf266f
alexsyareuu-web <alexsyareuu@gmail.com>
2025-10-16 10:36:33 +0700

    new

---
+++ Tugas1Jobsheet7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+    public interface Tugas1Jobsheet7 {
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int totalTiket = 0;
+        int totalPendapatan = 0;
+        
+        while (true) {
+           System.out.print("Masukkan jumlah tiket yang dibeli (atau 0 untuk keluar): ");
+           int jumlahTiket = input.nextInt();
+            if (jumlahTiket == 0) {
+            break;
+            } else if (jumlahTiket < 0){
+continue;
+} int hargaTiket = 50000;
+            if (jumlahTiket > 10) {
+        hargaTiket *= 0.85;
+            } else if (jumlahTiket > 4) {
+            hargaTiket *= 0.9;
+            } totalTiket += jumlahTiket;
+           totalPendapatan += hargaTiket * jumlahTiket;
+           System.out.println("Total tiket terjual: " + totalTiket);
+           System.out.println("Total pendapatan: Rp " + totalPendapatan);
+        } input.close();
+    }
+}
